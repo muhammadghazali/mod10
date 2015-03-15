3b4f276b654e2ea9a564b0c0d7f2e263ce40fe5e
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2015-03-15 17:27:28 +0700

    docs(*): update Readme
    so the users of this module will be aware of what Luhn can do and cannot do for them

--- test/test.js
+++ test/test.js
@@ -3,18 +3,38 @@
 var expect = require('expect.js');
 var mod10 = require('../');
 
-describe('Test valid credit card number', function() {
-  it('should return true', function() {
-    var validCC = 30569309025904;
+describe('Test credit card numbers', function() {
+  describe('Test valid credit card number', function() {
+    it('should return true', function() {
+      var validCC = 30569309025904;
 
-    expect(mod10(validCC)).to.be.ok();
+      expect(mod10(validCC)).to.be.ok();
+    });
+  });
+
+  describe('Test invalid credit card number', function() {
+    it('should return false', function() {
+      var invalidCC = 30569309825904;
+
+      expect(mod10(invalidCC)).to.not.be.ok();
+    });
   });
 });
 
-describe('Test invalid credit card number', function() {
-  it('should return true', function() {
-    var invalidCC = 30569309825904;
+describe('IMEI numbers', function() {
+  describe('# Test valid IMEI number', function() {
+    it('should return true', function() {
+      var validIMEI = 490154203237518;
 
-    expect(mod10(invalidCC)).to.not.be.ok();
+      expect(mod10(validIMEI)).to.be.ok();
+    });
+  });
+
+  describe('# Test invalid IMEI number', function() {
+    it('should return false', function() {
+      var invalidIMEI = 355873847028122;
+
+      expect(mod10(invalidIMEI)).to.not.be.ok();
+    });
   });
 });
